f3fc7313ea9386bfd36edac5c0916e18cf95ddc2
sorawat38 <st.sorawat38@gmail.com>
2024-11-22 16:53:30 -0700

    init commit

---
+++ InlandMarinaMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace InlandMarinaMVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
